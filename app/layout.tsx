5e32a717a777818c3b52007a3ee95e867ac06b85
sitemakes <kazuki.02.15.34.20@gmail.com>
2026-05-20 23:49:07 +0900

    Shorten page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "高校生向けオンライン学習サポート",
+  title: "学習サポート",
   description: "LINEで気軽に相談できる高校生向け学習サポートサービス"
 };
 
